da44da8c650b5fc6d6da532e6bc118ea39334c80
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2022-08-29 17:43:28 -0400

    Ability to Check Off Tasks

--- pages/api/db/todo-item.ts
+++ pages/api/db/todo-item.ts
@@ -7,7 +7,7 @@ type ResponseData = {
   data?: any,
 }
 
-const CreateTodoList = async (
+const CreateTodoItem = async (
     req: WithAuthProp<NextApiRequest>,
     res: NextApiResponse<ResponseData>
 ) => {
@@ -55,12 +55,94 @@ const CreateTodoList = async (
     }
 }
 
+const UpdateTodoItem = async (
+    req: WithAuthProp<NextApiRequest>,
+    res: NextApiResponse<ResponseData>
+) => {
+    const { userId } = req.auth;
+    const { id, completed } = req.body;
+
+    if (!userId) {
+        res.status(401).json({ message: "Please login to update a todo list." })
+        return;
+    }
+
+    if (!id || completed === undefined || completed === null) {
+        res.status(400).json({ message: "Please provide the id of the task to update and the value to update." })
+        return;
+    }
+
+    try {
+        await prisma.todoItem.updateMany({
+            where: {
+                id,
+                list: {
+                    owner_id: userId,
+                },
+            },
+            data: {
+                completed,
+            }
+        });
+
+        res.status(200).json({ message: "Task updated", data: { completed } });
+        return;
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({ message: "An error occurred when creating your list, please try again." });
+        return;
+    }
+}
+
+const GetAllTodoItems = async (
+    req: WithAuthProp<NextApiRequest>,
+    res: NextApiResponse<ResponseData>
+) => {
+    const { userId } = req.auth;
+    console.log(req.query)
+    const { list_id } = req.body;
+
+    if (!userId) {
+        res.status(401).json({ message: "Please login to update a todo list." })
+        return;
+    }
+
+    if (!list_id) {
+        res.status(400).json({ message: "Please provide the list id to get the tasks from." })
+        return;
+    }
+
+    try {
+        const tasks = await prisma.todoItem.findMany({
+            where: {
+                list_id
+            },
+        });
+
+        res.status(200).json({ message: "Tasks Retrieved", data: tasks });
+        return;
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({ message: "An error occurred when creating your list, please try again." });
+        return;
+    }
+}
+
 const handler = withAuth(async (
     req: WithAuthProp<NextApiRequest>,
     res: NextApiResponse<ResponseData>
-) => { 
+) => {
     if (req.method === 'POST') {
-        await CreateTodoList(req, res);
+        await CreateTodoItem(req, res);
+        return;
+    }
+    if (req.method === 'PUT') {
+        await UpdateTodoItem(req, res);
+        return;
+    }
+    // TODO: This is really a "GET" statement but "GET" doesn't allow us to pass the list variable to our function
+    if (req.method === 'PATCH') {
+        await GetAllTodoItems(req, res);
         return;
     }
 });
--- pages/list.tsx
+++ pages/list.tsx
@@ -2,7 +2,7 @@ import { GetServerSideProps, NextPage } from "next";
 import prisma from "lib/prisma";
 import { withServerSideAuth } from "@clerk/nextjs/ssr";
 import { TodoItem, TodoList } from "@prisma/client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
@@ -54,6 +54,8 @@ interface ListProp {
 }
 
 const List: NextPage<ListProp> = ({ list, todos }) => {
+    const [tasks, setTasks] = useState<TodoItem[]>(todos);
+
     const router = useRouter();
 
     const deleteList = async () => {
@@ -68,16 +70,62 @@ const List: NextPage<ListProp> = ({ list, todos }) => {
         }
     }
 
+    const fetchTasks = async () => {
+        const res = await fetch("/api/db/todo-item", {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                list_id: list.id,
+            }),
+        });
+
+        const json = await res.json();
+
+        setTasks(json.data);
+    }
+
+    const clickCheckBox = async (thisTask: TodoItem) => {
+        // Make click appear instantly to give good user feedback
+        const updatedTasks = tasks;
+        const updatedThisTask = thisTask;
+        updatedThisTask.completed = !thisTask.completed;
+        updatedTasks[tasks.indexOf(thisTask)] = updatedThisTask;
+
+        // TODO: Fix Bug - Have to stringify then parse because updatedTasks doesn't work
+        setTasks(JSON.parse(JSON.stringify(updatedTasks)));
+
+        const res = await fetch("/api/db/todo-item", {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: thisTask.id,
+                completed: updatedThisTask.completed,
+            }),
+        });
+
+        // Fetch the actual tasks and show the result
+        await fetchTasks();
+    }
+
+    useEffect(() => {
+        console.log(tasks)
+    }, [tasks])
+
     return (
         <div>
             <h1>{list.title}</h1>
             <button onClick={deleteList}>Delete List</button>
             <Link href={`/new-todo?id=${list.id}`}><a>Create New Todo</a></Link>
             <ol>
-                {todos.map((task: TodoItem) => {
+                {tasks.map((task: TodoItem) => {
                     return (
                         <li key={task.id}>
-                            <p>{task.task}</p>
+                            <label htmlFor={`checkbox-${task.id}`}>{task.task}</label>
+                            <input id={`checkbox-${task.id}`} type="checkbox" checked={task.completed} onChange={() => clickCheckBox(task)} />
                         </li>
                     )
                 })}
