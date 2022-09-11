29cecf703cf23a9c3f873316b843e3497d77e4d1
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2022-09-11 11:43:31 -0400

    Custom Email/Password Sign Flow is Now Interactive

--- components/pages/Auth.tsx
+++ components/pages/Auth.tsx
@@ -5,20 +5,61 @@ import Link from "next/link";
 import { AuthProp, AuthType, ListsProp } from 'lib/types';
 import { NextPage } from 'next';
 import { AuthenticateWithRedirectCallback, RedirectToUserProfile, SignedIn, SignedOut, UserButton, useSignIn, useSignUp, useUser } from '@clerk/nextjs';
-import React, { Children, DetailedHTMLProps, InputHTMLAttributes, ReactElement, ReactNode, useEffect } from 'react';
+import React, { Children, DetailedHTMLProps, InputHTMLAttributes, ReactElement, ReactEventHandler, ReactNode, useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { SiApple, SiGoogle, SiMicrosoft } from 'react-icons/si';
 import { IconType } from 'react-icons';
 import { IoMdArrowRoundBack } from 'react-icons/io'
 import { OAuthStrategy } from '@clerk/backend-core';
+import { ClerkAPIError } from '@clerk/types';
+import readingTime from "reading-time";
+
+interface AuthProps {
+    name: string;
+    email: string;
+    password: string;
+}
+
+interface Numbers {
+    digit0: number | null;
+    digit1: number | null;
+    digit2: number | null;
+    digit3: number | null;
+    digit4: number | null;
+    digit5: number | null;
+}
+
+const emptyCode: Numbers = {
+    digit0: null, 
+    digit1: null, 
+    digit2: null, 
+    digit3: null, 
+    digit4: null, 
+    digit5: null
+}
+
+enum CodeStatus {
+    Success,
+    Error,
+}
 
 const AuthPage: NextPage<AuthProp> = ({ type }) => {
+    const [form, setForm] = useState<AuthProps>({ name: "", email: "", password: "" });
+    const [code, setCode] = useState<Numbers>(emptyCode);
+    const [error, setError] = useState<string | null>(null);
+    const [verifyError, setVerifyError] = useState<string | null>(null);
+    const [disabled, setDisabled] = useState<boolean>(false);
+    const [signUpAttempt, setSignUp] = useState(null);
+    const [codeStatus, setCodeStatus] = useState<CodeStatus | null>(null);
+
+    const digit0Ref = useRef();
+
     const router = useRouter();
 
     let action = "";
     let actionCaps = "";
-    let auth = null;
+    let auth: any = null;
 
     if (type === AuthType.SignUp) {
         action = "Sign up";
@@ -30,28 +71,190 @@ const AuthPage: NextPage<AuthProp> = ({ type }) => {
         auth = useSignIn();
     }
 
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const target = e.currentTarget;
+        const name = target.name;
+
+        setForm({
+            ...form,
+            [name]: target.value,
+        })
+    }
+
+    const handleAuthAction = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (type === AuthType.SignUp) {
+            setError(null);
+
+            const [firstName, lastName] = form.name.split(' ');
+
+            try {
+                setSignUp(null);
+
+                const signUpAttempt = await auth.signUp.create({
+                    firstName,
+                    lastName,
+                    email_address: form.email,
+                    password: form.password,
+                });
+
+                const emailVerification = await signUpAttempt.prepareEmailAddressVerification();
+                setSignUp(emailVerification);
+            } catch (e) {
+                setError((e as { errors: ClerkAPIError[] }).errors[0].message);
+            }
+        } else if (type === AuthType.SignIn) {
+            try {
+                const res = await auth.signIn.create({
+                    identifier: form.email,
+                    password: form.password,
+                })
+                auth.setSession(res.createdSessionId, () => { router.push("/") });
+            } catch (e) {
+                console.log(e)
+            }
+        } else {
+            throw "Can't handle auth due to auth type not being set";
+        }
+    }
+
+    const handleBackButton = () => {
+        if (signUpAttempt) {
+            setSignUp(null);
+        } else {
+            router.push("/");
+        }
+    }
+
+    useEffect(() => {
+        (async () => {
+            const isCodeComplete = !Object.values(code).some(x => x === null);
+
+            if (isCodeComplete) {
+                setDisabled(true);
+
+                let stringCode = "";
+                Object.keys(code).forEach(key => {
+                    stringCode += code[key as keyof typeof code]?.toString();
+                })
+
+                let numberCode = parseInt(stringCode);
+
+                try {
+                    if (signUpAttempt) {
+                        const signUpVerification = await signUpAttempt.attemptEmailAddressVerification({ code: numberCode.toString() });
+                        setCodeStatus(CodeStatus.Success);
+
+                        await new Promise(callback => setInterval(callback, 1500));
+
+                        auth.setSession(signUpVerification.createdSessionId, () => { router.push("/") });
+                    } else {
+                        throw "SignUpAttempt object is null"; 
+                    }
+                } catch (e) {
+                    const error = (e as { errors: ClerkAPIError[] }).errors[0];
+                    
+                    setVerifyError(error.longMessage || null);
+                    setCodeStatus(CodeStatus.Error);
+
+                    await new Promise(callback => setTimeout(callback, readingTime(error.longMessage || "").time + 500));
+
+                    setVerifyError(null);
+                    setCodeStatus(null)
+                    setCode(emptyCode);
+                    setDisabled(false);
+                }
+            }
+        })()
+    }, [code])
+
+    useEffect(() => {
+        if (!disabled) {
+            // @ts-ignore
+            digit0Ref.current?.focus();
+        }
+    }, [disabled])
+
     return (
         <>
-            <div id={style.backButton} onClick={() => {router.push("/")}}><IoMdArrowRoundBack /> <span>Back</span></div>
+            <div id={style.backButton} onClick={handleBackButton}><IoMdArrowRoundBack /> <span>Back</span></div>
             <SignedOut>
-                <div className={style.container}>
-                    <div id={style.infoText}>
-                        <h1>{actionCaps}</h1>
-                        <h2>Create a new account for njf lists.</h2>
+                {!signUpAttempt &&
+                    <div className={style.container}>
+                        <div id={style.infoText}>
+                            <h1>{actionCaps}</h1>
+                            <h2>Create a new account for njf lists.</h2>
+                        </div>
+                        <AuthProviders action={action} auth={auth} type={type} />
+                        <div id={style.dividerContainer}>
+                            <hr/>
+                            <span>or</span>
+                            <hr/>
+                        </div>
+                        <form onSubmit={handleAuthAction} id={style.manualAuthContainer}>
+                            {type === AuthType.SignUp && <AuthInput placeholder='John Doe' id="name" name="name" pattern="^[a-zA-Z]+ [a-zA-Z]+$" value={form.name} onChange={handleInputChange}>Full Name</AuthInput>}
+                            <AuthInput type="email" placeholder='example@mail.com' id="email" name="email" value={form.email} onChange={handleInputChange}>Email</AuthInput>
+                            <AuthInput type="password" placeholder='At least 8 characters' id="password" name="password" value={form.password} onChange={handleInputChange}>Password</AuthInput>
+                            {error && <span id={style.errorMessage}>{error}</span>}
+                            <input id={style.submitButton} type="submit" value={actionCaps} />
+                        </form>
+                    </div> ||
+                    <div className={style.container}>
+                        <h1>Verify Your Email</h1>
+                        <h2>Enter the code sent to your email.</h2>
+                        <div id={style.authCodeContainer}>
+                            {
+                                Array.from(Array(6).keys()).map((number) => {
+                                    const indexValue = `digit${number.toString()}` as keyof typeof code;
+
+                                    // @ts-ignore
+                                    return <input ref={number === 0 ? digit0Ref : undefined}
+                                        autoFocus={number === 0} disabled={disabled}
+                                        pattern='\d*'
+                                        id={indexValue}
+                                        key={number}
+                                        className={style.authCodeInput}
+                                        // @ts-ignore
+                                        value={code[indexValue] !== null ? code[indexValue] : ""}
+                                        style={{ borderColor: codeStatus !== null ? codeStatus === CodeStatus.Success ? "#00bb00" : "red" : "transparent" }}
+                                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                            e.preventDefault();
+
+                                            const codeNumber = parseInt(e.target.value);
+                                            
+                                            const afterValue = Number.isNaN(codeNumber) ? null : codeNumber.toString().length <= 1 ? codeNumber : parseInt(codeNumber.toString().slice(1,2));
+                                            setCode({ ...code, [indexValue]: afterValue });
+
+                                            if (!/^[0-9]+$/.test(e.target.value)) { return; }
+                                            // @ts-ignore
+                                            e.target.nextElementSibling?.focus();
+                                        }} 
+                                        onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
+                                            if (e.key === "Backspace" && code[indexValue] === null) {
+                                                // @ts-ignore
+                                                e.target.previousSibling?.focus();
+                                            } else if (e.key === "ArrowRight" || e.key === "ArrowDown") {
+                                                e.preventDefault();
+                                                // @ts-ignore
+                                                e.target.nextElementSibling?.focus();
+                                            } else if (e.key === "ArrowLeft" || e.key === "ArrowUp") {
+                                                e.preventDefault();
+                                                // @ts-ignore
+                                                e.target.previousSibling?.focus();
+                                            }
+                                        }}
+                                        onMouseDown={(e: React.MouseEvent<HTMLInputElement>) => {
+                                            e.preventDefault();
+                                            e.currentTarget.focus()
+                                            e.currentTarget.setSelectionRange(e.currentTarget.value.length, e.currentTarget.value.length);
+                                        }} />
+                                })
+                            }
+                        </div>
+                        <p id={style.verifyErrorText}>{verifyError}</p>
                     </div>
-                    <AuthProviders action={action} auth={auth} type={type} />
-                    <div id={style.dividerContainer}>
-                        <hr/>
-                        <span>or</span>
-                        <hr/>
-                    </div>
-                    <div id={style.manualAuthContainer}>
-                        {type === AuthType.SignUp && <AuthInput placeholder='John Doe'>Full Name</AuthInput>}
-                        <AuthInput type="email" placeholder='example@mail.com'>Email</AuthInput>
-                        <AuthInput type="password" placeholder='At least 8 characters'>Password</AuthInput>
-                        <input id={style.submitButton} type="submit" value={actionCaps} />
-                    </div>
-                </div>
+                }
             </SignedOut>
             <SignedIn>
                 <RedirectToUserProfile />
@@ -110,11 +313,11 @@ const AuthButton: NextPage<{ action: string, provider: string, providerIcon: Ico
     )
 }
 
-const AuthInput: NextPage<DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>> = ({ children, placeholder, type }) => {
+const AuthInput: NextPage<DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>> = ({ children, placeholder, type, id, value, onChange, name, pattern }) => {
     return (
         <div className={style.inputContainer}>
-            <h3>{children}</h3>
-            <input type={type} placeholder={placeholder} />
+            <label htmlFor={id}>{children}</label>
+            <input id={id} type={type} minLength={type === "password" ? 8 : undefined} placeholder={placeholder} value={value} onChange={onChange} name={name} required pattern={pattern} />
         </div>
     )
 }
