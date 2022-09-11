c804a76b6d2be0bfd8aef4d8cacdea043b48d598
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2022-09-11 17:41:23 -0400

    Setup Open Replay

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,17 +6,26 @@ import PageLayout from 'components/Layouts/PageLayout';
 import { init } from '@socialgouv/matomo-next'
 import { useEffect } from 'react';
 import Head from 'next/head';
+import Tracker from "@openreplay/tracker";
 
 // Using the or operator so Typescript doesn't complain because env variables might be undefined sometimes
 const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL || '';
 const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID || '';
 
+const tracker = new Tracker({
+  projectKey: process.env.NEXT_PUBLIC_OPEN_REPLAY_PROJECT_KEY || '',
+  __DISABLE_SECURE_MODE: process.env.NODE_ENV === "development",
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   let hasRun = false;
 
   useEffect(() => {
+    console.log("lol")
     if (!hasRun) {
       init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
+
+      tracker.start();
 
       hasRun = true;
     }
