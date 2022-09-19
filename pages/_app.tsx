2b35d9e7f80eb932f31eed0cd47d25d339c5ec1f
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2022-09-18 20:53:05 -0400

    Add Highlight and Send Session URL to Sentry

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import 'styles/globals.css';
 import 'styles/clerk.css';
 import type {AppProps} from 'next/app'
-import {ClerkProvider, useAuth} from '@clerk/nextjs'
+import {ClerkProvider, useAuth, useUser} from '@clerk/nextjs'
 import PageLayout from 'components/Layouts/PageLayout';
 import {init} from '@socialgouv/matomo-next'
 import {useEffect} from 'react';
@@ -9,12 +9,35 @@ import Head from 'next/head';
 import Tracker from "@openreplay/tracker";
 import {AppRouter} from "pages/api/trpc/[trpc]";
 import {withTRPC} from "@trpc/next";
+import {H} from "highlight.run";
+import {NextPage} from "next";
+import {ErrorBoundary} from "@highlight-run/react";
+import * as Sentry from "@sentry/nextjs";
 
 export { reportWebVitals } from 'next-axiom';
 
 // Using the or operator so Typescript doesn't complain because env variables might be undefined sometimes
 const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL || '';
 const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID || '';
+
+if (process.env.NODE_ENV !== "development") {
+    H.init(process.env.NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID, {
+        tracingOrigins: true,
+        networkRecording: {
+            enabled: true,
+            recordHeadersAndBody: true
+        },
+        environment: process.env.VERCEL_ENV === "preview" ?
+            "staging" :
+            process.env.NODE_ENV,
+    });
+
+    H.getSessionURL().then(sessionUrl => {
+        Sentry.setContext("highlight", {
+            "url": sessionUrl
+        })
+    })
+}
 
 const tracker = new Tracker({
     projectKey: process.env.NEXT_PUBLIC_OPEN_REPLAY_PROJECT_KEY || '',
@@ -35,14 +58,35 @@ function MyApp({Component, pageProps}: AppProps) {
     }, []);
 
     return (
-        <ClerkProvider>
-            <Head>
-                <meta name='viewport' content='initial-scale=1, viewport-fit=cover'/>
-            </Head>
+        <ErrorBoundary>
+            <ClerkProvider>
+                <Head>
+                    <meta name='viewport' content='initial-scale=1, viewport-fit=cover'/>
+                </Head>
 
-            <Component {...pageProps} />
-        </ClerkProvider>
+                <AuthStateHandler/>
+
+                <Component {...pageProps} />
+            </ClerkProvider>
+        </ErrorBoundary>
     );
+}
+
+const AuthStateHandler: NextPage = () => {
+    const user = useUser();
+
+    useEffect(() => {
+        if (user.isSignedIn) {
+            H.identify(user?.user?.primaryEmailAddress?.emailAddress ?? "", {
+                id: user.user.id,
+                first_name: user.user.firstName ?? "",
+                last_name: user.user.lastName ?? "",
+                avatar: user.user.profileImageUrl
+            })
+        }
+    }, [user]);
+
+    return <></>;
 }
 
 export default withTRPC<AppRouter>({
